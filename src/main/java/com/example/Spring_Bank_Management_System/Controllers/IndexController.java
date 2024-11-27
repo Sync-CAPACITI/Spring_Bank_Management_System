c593bcd4c407725e701bc85d7901d8d8ab1a6af3
NuffSaid-Bore <borekamohelo@gmail.com>
2024-11-27 12:02:41 +0200

    added Index controller

---
+++ src/main/java/com/example/Spring_Bank_Management_System/Controllers/IndexController.java
@@ -0,0 +1,25 @@
+package com.example.Spring_Bank_Management_System.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+     @GetMapping("/")
+    public ModelAndView getIndex(){
+        ModelAndView getIndexPage = new ModelAndView("index");
+        getIndexPage.addObject("PageTitle", "Home");
+        System.out.println("In Thre Landing Page Controller");
+        return getIndexPage;
+    }
+
+
+    @GetMapping("/error")
+    public ModelAndView getError(){
+        ModelAndView getErrorPage = new ModelAndView("error");
+        getErrorPage.addObject("PageTitle", "Errors");
+        System.out.println("In Error Page Controller");
+        return getErrorPage;
+    }
+}
