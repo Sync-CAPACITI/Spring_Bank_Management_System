8e66211f1991340bece4c445a7f4f314af40d6f8
NuffSaid-Bore <borekamohelo@gmail.com>
2024-11-27 09:24:14 +0200

    Initial Commit

---
+++ src/main/java/com/example/Spring_Bank_Management_System/SpringBankManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Bank_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBankManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBankManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_Bank_Management_System/SpringBankManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Bank_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBankManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
